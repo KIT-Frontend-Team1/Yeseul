0b4241aba06063b6e26b6874f980d13ee1c1fcaa
yesoryeseul <yesoryeseul07@gmail.com>
2023-06-06 23:02:58 +0900

    axiostodo: addTodo 로직 api call 관심사 분리 추가 수정

--- lecture-study/axiostodo/src/hooks/crud/use-add.js
+++ lecture-study/axiostodo/src/hooks/crud/use-add.js
@@ -1,14 +1,19 @@
 import { axiosInstance } from "utils/axios";
 import { useState, useEffect } from "react";
+import { addTodoApi, getTodoApi } from "./api.todo";
 
 const useAdd = (endpoint) => {
   const [todoList, setTodoList] = useState([]);
 
   const getTodoList = async () => {
     try {
-      const res = await axiosInstance.get(endpoint);
-      console.log(res);
-      setTodoList(res.data.data);
+      // const res = await axiosInstance.get(endpoint);
+      // console.log(res);
+      // setTodoList(res.data.data);
+
+      // api call 관심사 분리
+      const resData = await getTodoApi(endpoint);
+      setTodoList(resData);
     } catch (err) {
       console.error(err);
     }
@@ -22,17 +27,20 @@ const useAdd = (endpoint) => {
   // async는 Promise를 리턴하므로 안에 리턴 써주지 않아도 됨
   const addTodo = async (title, content) => {
     try {
-      if (!title || !content) {
-        const err = new Error();
-        err.type = "empty content";
-        err.message = "빈칸을 채워주세요";
-        throw err;
-      }
+      // if (!title || !content) {
+      //   const err = new Error();
+      //   err.type = "empty content";
+      //   err.message = "빈칸을 채워주세요";
+      //   throw err;
+      // }
 
-      await axiosInstance.post(endpoint, {
-        title,
-        content,
-      });
+      // await axiosInstance.post(endpoint, {
+      //   title,
+      //   content,
+      // });
+
+      // api call 관심사 분리
+      await addTodoApi(endpoint, title, content);
       getTodoList();
     } catch (err) {
       throw err;
