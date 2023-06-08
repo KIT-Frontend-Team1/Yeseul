ece2098ea2d35af06202d793e94c30b0d9ba3458
yesoryeseul <yesoryeseul07@gmail.com>
2023-06-08 17:13:25 +0900

    axiostodo: api 분리 리팩터링

--- lecture-study/axiostodo/src/hooks/use-crud.js
+++ lecture-study/axiostodo/src/hooks/use-crud.js
@@ -7,15 +7,11 @@ const useCrud = () => {
 
   // 조회 함수
   const getTodoList = async () => {
-    try {
-      // const res = await axiosInstance.get("/todo");
-      // setTodoList(res.data.data);
+    // const res = await axiosInstance.get("/todo");
+    // setTodoList(res.data.data);
 
-      // api call 관심사 분리
-      await apis.getApi();
-    } catch (err) {
-      console.error(err);
-    }
+    // api call 관심사 분리
+    await apis.getApi();
   };
 
   useEffect(() => {
@@ -25,47 +21,28 @@ const useCrud = () => {
   // 추가 함수
   // async는 Promise를 리턴하므로 안에 리턴 써주지 않아도 됨
   const addTodo = async (title, content) => {
-    try {
-      if (!title || !content) {
-        const err = new Error();
-        err.type = "empty content";
-        err.message = "빈칸을 채워주세요";
-        throw err;
-      }
+    // await axiosInstance.post("/todo", {
+    //   title,
+    //   content,
+    // });
 
-      // await axiosInstance.post("/todo", {
-      //   title,
-      //   content,
-      // });
-
-      // api call 관심사 분리
-      await apis.addApi(title, content);
-    } catch (err) {
-      throw err;
-    }
+    // api call 관심사 분리
+    await apis.addApi(title, content);
   };
 
   // 수정, 체크 함수
   const handleUpdateTodo = async (id, content, state) => {
-    try {
-      // await axiosInstance.put(`/todo/${id}`, { id, content, state });
+    // await axiosInstance.put(`/todo/${id}`, { id, content, state });
 
-      // api call 관심사 분리
-      await apis.updateApi(id, content, state);
-    } catch (err) {
-      console.error("Error!");
-    }
+    // api call 관심사 분리
+    await apis.updateApi(id, content, state);
   };
 
   const handleDeleteTodo = async (id) => {
-    try {
-      // await axiosInstance.delete(`/todo/${id}`, { id });
+    // await axiosInstance.delete(`/todo/${id}`, { id });
 
-      // api call 관심사 분리
-      await apis.deleteApi(id);
-    } catch (err) {
-      console.log(err);
-    }
+    // api call 관심사 분리
+    await apis.deleteApi(id);
   };
 
   return {
